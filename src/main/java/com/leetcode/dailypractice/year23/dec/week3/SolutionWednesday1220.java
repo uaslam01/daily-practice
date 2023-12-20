72922c77b8f473f951aa7affe391bb350886d9ce
Ubaidullah <ubaidullahaslam1@gmail.com>
2023-12-20 21:48:05 +0500

    Update SolutionWednesday1220.java

--- src/main/java/com/leetcode/dailypractice/year23/dec/week3/SolutionWednesday1220.java
+++ src/main/java/com/leetcode/dailypractice/year23/dec/week3/SolutionWednesday1220.java
@@ -60,7 +60,7 @@ public class SolutionWednesday1220 {
 		return (money-secondSmall-firstSmall)<0?money:(money-secondSmall-firstSmall);
 	}
 	//Best Solution
-    public int buyChoco(int[] prices, int money) {
+    public int buyChoco2(int[] prices, int money) {
         // Assume minimum and second minimum
         int minimum = Math.min(prices[0], prices[1]);
         int secondMinimum = Math.max(prices[0], prices[1]);
@@ -87,7 +87,53 @@ public class SolutionWednesday1220 {
         // We cannot buy chocolates. Return the initial amount of money
         return money;
     }
+	//Best Solution 2
+    //Using counting sort approach
+    public int buyChoco(int[] prices, int money) {
+        // Array to store frequency of prices
+        int[] freq = new int[101];
+        for (int p : prices) {
+            freq[p]++;
+        }
 
+        // Assume minimum and second minimum to be zero.
+        // Since prices[i] cannot be 0, the 0 value implies
+        //They haven't been computed yet.
+        int minimum = 0;
+        int secondMinimum = 0;
+        for (int price = 1; price <= 100; price++) {
+            if (freq[price] > 1) {
+                minimum = price;
+                secondMinimum = price;
+                break;
+            } else if (freq[price] == 1) {
+                minimum = price;
+                break;
+            }
+        }
+
+        // If the second minimum is not found, then find it
+        if (secondMinimum == 0) {
+            for (int price = minimum + 1; price <= 100; price++) {
+                if (freq[price] > 0) {
+                    secondMinimum = price;
+                    break;
+                }
+            }
+        }
+
+        // Minimum Cost
+        int minCost = minimum + secondMinimum;
+
+        // We can buy chocolates only if we have enough money
+        if (minCost <= money) {
+            // Return the Amount of Money Left
+            return money - minCost;
+        }
+
+        // We cannot buy chocolates. Return the initial amount of money
+        return money;
+    }
 
 	public static void main(String[] args) {
 		Consumer cons = System.out::println;
