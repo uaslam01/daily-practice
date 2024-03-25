9a0e9ec4499d5cebb36fe0bffa5ecacc39efedf0
Ubaidullah <ubaidullahaslam1@gmail.com>
2024-03-25 23:02:22 +0500

    Update SolutionMonday0326.java

--- src/main/java/com/leetcode/dailypractice/year24/mar/week4/SolutionMonday0326.java
+++ src/main/java/com/leetcode/dailypractice/year24/mar/week4/SolutionMonday0326.java
@@ -76,26 +76,29 @@ public class SolutionMonday0326 {
 
     //Best Solution 2
     private List<Integer> res;
+
     public List<Integer> findDuplicates(int[] nums) {
         return new AbstractList<Integer>() {
             public Integer get(int index) {
                 init();
                 return res.get(index);
             }
+
             public int size() {
                 init();
                 return res.size();
             }
+
             private void init() {
-                if(res != null) return;
+                if (res != null) return;
                 res = new ArrayList<>();
                 int t;
-                for(int i=0; i<nums.length; i++) {
+                for (int i = 0; i < nums.length; i++) {
                     t = Math.abs(nums[i]);
-                    if(nums[t-1] < 0) {
+                    if (nums[t - 1] < 0) {
                         res.add(t);
                     } else {
-                        nums[t-1] *= -1;
+                        nums[t - 1] *= -1;
                     }
                 }
             }
