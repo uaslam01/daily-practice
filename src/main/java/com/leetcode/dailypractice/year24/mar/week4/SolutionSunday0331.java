f6780381a48876220479bf49718cb1674b4ca7ac
Ubaidullah <ubaidullahaslam1@gmail.com>
2024-03-31 14:26:35 +0500

    Create SolutionSunday0331.java

---
+++ src/main/java/com/leetcode/dailypractice/year24/mar/week4/SolutionSunday0331.java
@@ -0,0 +1,87 @@
+package com.leetcode.dailypractice.year24.mar.week4;
+
+/**
+ * <pre>
+ * <a href= "https://leetcode.com/problems/count-subarrays-with-fixed-bounds/">Problem-Link</a>
+ *  Count-subarrays-with-fixed-bounds
+ *
+ * You are given an integer array nums and two integers minK and maxK.
+ *
+ * A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
+ *
+ * The minimum value in the subarray is equal to minK.
+ * The maximum value in the subarray is equal to maxK.
+ * Return the number of fixed-bound subarrays.
+ *
+ * A subarray is a contiguous part of an array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+ * Output: 2
+ * Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: nums = [1,1,1,1], minK = 1, maxK = 1
+ * Output: 10
+ * Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 2 <= nums.length <= 105
+ *
+ * 1 <= nums[i], minK, maxK <= 106
+ */
+public class SolutionSunday0331 {
+    //Best Solution1
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long res = 0;
+        int badIdx = -1, leftIdx = -1, rightIdx = -1;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                badIdx = i;
+            }
+
+            if (nums[i] == minK) {
+                leftIdx = i;
+            }
+
+            if (nums[i] == maxK) {
+                rightIdx = i;
+            }
+
+            res += Math.max(0, Math.min(leftIdx, rightIdx) - badIdx);
+        }
+
+        return res;
+
+    }
+    public long countSubarrays1(int[] nums, int minK, int maxK) {
+        //Best Solution2
+        int minI = -1, maxI = -1, left = -1, right = 0;
+        long count = 0;
+        while(right < nums.length){
+            if(nums[right] < minK || nums[right] > maxK){
+                minI = right;
+                maxI = right;
+                left = right;
+            }
+            minI = nums[right] == minK ? right : minI;
+            maxI = nums[right] == maxK ?  right : maxI;
+            count += Math.min(minI, maxI) - left;
+            right++;
+        }
+        return count;
+    }
+
+
+
+}
