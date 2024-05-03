f78aa3b5483827830dd33e253aff53ef67bb93aa
Ubaidullah <ubaidullahaslam1@gmail.com>
2024-05-03 16:26:49 +0500

    Update SolutionFriday0503.java

--- src/main/java/com/leetcode/dailypractice/year24/may/week1/SolutionFriday0503.java
+++ src/main/java/com/leetcode/dailypractice/year24/may/week1/SolutionFriday0503.java
@@ -60,68 +60,68 @@ package com.leetcode.dailypractice.year24.may.week1;
  * Submissions 1.1M Acceptance Rate 37.7%
  **/
 public class SolutionFriday0503 {
-	//Mine Solution
+	// Mine Solution
 	public int compareVersion(String version1, String version2) {
 		String[] v1List = version1.split("\\.");
 		String[] v2List = version2.split("\\.");
-		
-		int longVersionLen = v1List.length>v2List.length?v1List.length:v2List.length;
-		int shortVersionLen = v1List.length>v2List.length?v2List.length:v1List.length;
-		String[] longVerList = v1List.length>v2List.length?v1List:v2List;
-		
-		int answer = v1List.length>v2List.length?1:-1;
-;
-		for(int i=0;i<shortVersionLen;i++) {
+
+		int longVersionLen = v1List.length > v2List.length ? v1List.length : v2List.length;
+		int shortVersionLen = v1List.length > v2List.length ? v2List.length : v1List.length;
+		String[] longVerList = v1List.length > v2List.length ? v1List : v2List;
+
+		int answer = v1List.length > v2List.length ? 1 : -1;
+		;
+		for (int i = 0; i < shortVersionLen; i++) {
 			int v1 = Integer.parseInt(v1List[i]);
 			int v2 = Integer.parseInt(v2List[i]);
-			if(v1<v2)
+			if (v1 < v2)
 				return -1;
-			else if(v1>v2)
+			else if (v1 > v2)
 				return 1;
 		}
-		
-		for(int i=shortVersionLen;i<longVersionLen;i++) {
+
+		for (int i = shortVersionLen; i < longVersionLen; i++) {
 			int v1 = Integer.parseInt(longVerList[i]);
-			if(v1!=0)
-				return answer; 
+			if (v1 != 0)
+				return answer;
 
 		}
 		return 0;
-		
-	}
-	//Best Solution
-    public int compareVersion1(String version1, String version2) {
-        int n = version1.length(), m = version2.length();
-        int i = 0, j = 0;
-        while (i < n || j < m) {
-            int x = 0;
-            for (; i < n && version1.charAt(i) != '.'; ++i) {
-                x = x * 10 + version1.charAt(i) - '0';
-            }
-            ++i; // 跳过点号
-            int y = 0;
-            for (; j < m && version2.charAt(j) != '.'; ++j) {
-                y = y * 10 + version2.charAt(j) - '0';
-            }
-            ++j; // 跳过点号
-            if (x != y) {
-                return x > y ? 1 : -1;
-            }
-        }
-        return 0;
-    }
-	
-	public static void main(String[] arg) {
-		var obj = new SolutionFriday0503();
-		System.out.println(obj.compareVersion("1.01","1.001"));
-		System.out.println(obj.compareVersion("1.0","1.0.0"));
-		System.out.println(obj.compareVersion("0.1","1.1"));
-		//Custom Input
-		System.out.println(obj.compareVersion("10.1.11.01.2","10.1.11.01.2"));
-		System.out.println(obj.compareVersion("10.1.11.01.2","10.11.11."));
-		System.out.println(obj.compareVersion("10","10.00"));
 
 	}
-	
+
+	// Best Solution
+	public int compareVersion1(String version1, String version2) {
+		int n = version1.length(), m = version2.length();
+		int i = 0, j = 0;
+		while (i < n || j < m) {
+			int x = 0;
+			for (; i < n && version1.charAt(i) != '.'; ++i) {
+				x = x * 10 + version1.charAt(i) - '0';
+			}
+			++i; // 跳过点号
+			int y = 0;
+			for (; j < m && version2.charAt(j) != '.'; ++j) {
+				y = y * 10 + version2.charAt(j) - '0';
+			}
+			++j; // 跳过点号
+			if (x != y) {
+				return x > y ? 1 : -1;
+			}
+		}
+		return 0;
+	}
+
+	public static void main(String[] arg) {
+		var obj = new SolutionFriday0503();
+		System.out.println(obj.compareVersion("1.01", "1.001"));
+		System.out.println(obj.compareVersion("1.0", "1.0.0"));
+		System.out.println(obj.compareVersion("0.1", "1.1"));
+		// Custom Input
+		System.out.println(obj.compareVersion("10.1.11.01.2", "10.1.11.01.2"));
+		System.out.println(obj.compareVersion("10.1.11.01.2", "10.11.11."));
+		System.out.println(obj.compareVersion("10", "10.00"));
+
+	}
 
 }
