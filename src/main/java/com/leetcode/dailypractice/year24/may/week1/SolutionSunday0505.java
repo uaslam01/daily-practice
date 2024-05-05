203cf0efb7c4d76c04eaef74f52b7897b11a15e8
Ubaidullah <ubaidullahaslam1@gmail.com>
2024-05-05 20:06:23 +0500

    Today solution is added

---
+++ src/main/java/com/leetcode/dailypractice/year24/may/week1/SolutionSunday0505.java
@@ -0,0 +1,129 @@
+package com.leetcode.dailypractice.year24.may.week1;
+
+import java.util.function.Consumer;
+
+/**
+ * <pre>
+ * <a href= "https://leetcode.com/problems/delete-node-in-a-linked-list/">
+ * Problem-Link</a>
+ * 
+ * 237. Delete Node in a Linked List
+ * 
+ * Medium Topics Companies
+ * 
+ * There is a singly-linked list head and we want to delete a node node in it.
+ * 
+ * You are given the node to be deleted node. You will not be given access to
+ * the first node of head.
+ * 
+ * All the values of the linked list are unique, and it is guaranteed that the
+ * given node node is not the last node in the linked list.
+ * 
+ * Delete the given node. Note that by deleting the node, we do not mean
+ * removing it from memory. We mean:
+ * 
+ * The value of the given node should not exist in the linked list. The number
+ * of nodes in the linked list should decrease by one. All the values before
+ * node should be in the same order. All the values after node should be in the
+ * same order. 
+ * 
+ * <b>Custom testing:</b>
+ * 
+ * For the input, you should provide the entire linked list head and the node to
+ * be given node. node should not be the last node of the list and should be an
+ * actual node in the list. We will build the linked list and pass the node to
+ * your function. The output will be the entire list after calling your
+ * function.
+ * 
+ * 
+ * Example 1:
+ * 
+ * <img src="node1.jpg" width="50%"/>
+ * 
+ * Input: head = [4,5,1,9], node = 5 Output: [4,1,9] Explanation: You are given
+ * the second node with value 5, the linked list should become 4 -> 1 -> 9 after
+ * calling your function.
+ * 
+ * Example 2:
+ * 
+ * <img src="node2.jpg" width="50%"/>
+ * 
+ * 
+ * Input: head = [4,5,1,9], node = 1 Output: [4,5,9] Explanation: You are given
+ * the third node with value 1, the linked list should become 4 -> 5 -> 9 after
+ * calling your function.
+ * 
+ * 
+ * Constraints:
+ * 
+ * The number of the nodes in the given list is in the range [2, 1000]. -1000 <=
+ * Node.val <= 1000 The value of each node in the list is unique. The node to be
+ * deleted is in the list and is not a tail node.
+ * 
+ * Seen this question in a real interview before? 1/5 Yes No Accepted 1.3M
+ * Submissions 1.7M Acceptance Rate 79.3%
+ **/
+
+public class SolutionSunday0505 {
+	// Mine Solution/Best Solution 1
+	public void deleteNode(ListNode node) {
+		ListNode temp = node;
+		while (node.next != null) {
+			temp = node;
+			node.val = node.next.val;
+			node = node.next;
+
+		}
+		temp.next = null;
+	}
+
+	// Best Solution 2, although it was restricted to remove the value, but he
+	// jumped
+	// next to next
+	// for keeping the chain smooth
+	public void deleteNode1(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+		System.gc();
+	}
+
+	// Definition for singly-linked list.
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void main(String[] arg) {
+		var obj = new SolutionSunday0505();
+		Consumer<ListNode> cons = x -> {
+			while (x != null) {
+				System.out.print(x.val);
+				x = x.next;
+			}
+		};
+		ListNode temp = new ListNode(1, new ListNode(2));
+		ListNode temp1 = new ListNode(3, new ListNode(4));
+		temp.next.next = temp1;
+		temp1.next.next = new ListNode(5);
+		obj.deleteNode(temp1);
+		cons.accept(temp);
+		System.out.println("---");
+
+		obj.deleteNode(temp);
+		cons.accept(temp);
+		System.out.println("---");
+
+	}
+}
