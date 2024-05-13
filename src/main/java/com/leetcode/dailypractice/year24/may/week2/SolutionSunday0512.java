a41083a5f285f3b5402fe385b57011137ebf2ce3
Ubaidullah <ubaidullahaslam1@gmail.com>
2024-05-13 15:07:38 +0500

    Update SolutionSunday0512.java

--- src/main/java/com/leetcode/dailypractice/year24/may/week2/SolutionSunday0512.java
+++ src/main/java/com/leetcode/dailypractice/year24/may/week2/SolutionSunday0512.java
@@ -93,6 +93,26 @@ public class SolutionSunday0512 {
     	}
     	return max;
     }
+	
+	//Best Solution 2
+    public int[][] largestLocal2(int[][] grid) {
+        int[][] maxLocal = new int[grid.length - 2][grid.length - 2];
+
+        for(int i = 0; i < maxLocal.length; i++) {
+            for(int j = 0; j < maxLocal.length; j++) {
+                maxLocal[i][j] = 0;
+                for(int a = i; a < i + 3; a++) {
+                    for(int b = j; b < j + 3; b++){
+                        if(maxLocal[i][j] < grid[a][b]){
+                            maxLocal[i][j] = grid[a][b];
+                        }
+                    }
+                }
+            }
+        }
+        return maxLocal;
+
+    }
 	public static void main(String[] arg) {
 		var obj = new SolutionSunday0512();
 		IntConsumer cons = System.out::println;
