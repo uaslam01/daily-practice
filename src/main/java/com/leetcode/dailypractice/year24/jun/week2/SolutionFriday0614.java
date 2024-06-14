eda4478d621be5eb65cd1943931f19e1ef126903
Ubaidullah Muhammad Aslam <ubaidullahaslam1@gmail.com>
2024-06-14 23:01:36 +0500

    Create SolutionFriday0614.java

---
+++ src/main/java/com/leetcode/dailypractice/year24/jun/week2/SolutionFriday0614.java
@@ -0,0 +1,54 @@
+package com.leetcode.dailypractice.year24.jun.week2;
+/**
+ * <pre>
+ * <a href= "https://leetcode.com/problems/sort-colors/">Problem-Link</a>
+ *
+ * 75. Sort Colors
+ * <p>
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * <p>
+ * Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+ * <p>
+ * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+ * <p>
+ * You must solve this problem without using the library's sort function.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [2,0,1]
+ * Output: [0,1,2]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * n == nums.length
+ * 1 <= n <= 300
+ * nums[i] is either 0, 1, or 2.
+ * <p>
+ * <p>
+ * Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+ * <p>
+ * <p>
+ * Seen this question in a real interview before?
+ * 1/5
+ * Yes
+ * No
+ * Accepted
+ * 2M
+ * Submissions
+ * 3.2M
+ * Acceptance Rate
+ * 63.1%
+ */
+public class SolutionFriday0614 {
+}
