bc43d2cd42f07b6ba3a2f8f1049d72165c6dffb4
Ubaidullah Muhammad Aslam <ubaidullahaslam1@gmail.com>
2024-06-16 01:57:29 +0500

    Create SolutionSaturday0608.java

---
+++ src/main/java/com/leetcode/dailypractice/year24/jun/week3/SolutionSaturday0608.java
@@ -0,0 +1,68 @@
+package com.leetcode.dailypractice.year24.mar.week1;
+
+import java.util.function.Consumer;
+
+/**
+ * <pre>
+ * <a href= "https://leetcode.com/problems/linked-list-cycle/">Problem-Link</a>
+ *
+ * 141. Linked List Cycle
+ * 
+ * Easy Topics Companies
+ * 
+ * Given head, the head of a linked list, determine if the linked list has a
+ * cycle in it.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can
+ * be reached again by continuously following the next pointer. Internally, pos
+ * is used to denote the index of the node that tail's next pointer is connected
+ * to. Note that pos is not passed as a parameter.
+ * 
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * 
+ * 
+ * Example 1:
+ * 
+ * <img src="circularlinkedlist.png" width="80%"/>
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * 
+ * Output: true
+ * 
+ * Explanation: There is a cycle in the linked list, where the tail connects to
+ * the 1st node (0-indexed).
+ * 
+ * Example 2:
+ * 
+ * <img src="circularlinkedlist_test2.png" width="40%" >
+ * 
+ * Input: head = [1,2], pos = 0
+ * 
+ * Output: true
+ * 
+ * Explanation: There is a cycle in the linked list, where the tail connects to
+ * the 0th node.
+ * 
+ * 
+ * Example 3:
+ * 
+ * <img src="circularlinkedlist_test3.png" width="40%" height = "20%"/>
+ * 
+ * Input: head = [1], pos = -1
+ * 
+ * Output: false
+ * 
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * Constraints:
+ * 
+ * The number of the nodes in the list is in the range [0, 104]. -105 <=
+ * Node.val <= 105 pos is -1 or a valid index in the linked-list.
+ * 
+ * 
+ */
+public class SolutionSaturday0608 {
+
+
+}
